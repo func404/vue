032d69581fcc0b38ed3681c8b87198163ab6fafe
Jingxinpo <492971588@qq.com>
2018-12-21 11:28:07 +0800

    vue

---
+++ src/views/Test.vue
@@ -0,0 +1,36 @@
+
+  <template >
+   <div>
+      {{ message }}
+
+      <section>
+          <button v-on:click="resolve">resolve</button>
+      </section>
+      <section>
+         <HelloWorld></HelloWorld>
+      </section>
+      <div v-bind:id="dynamicId">rewrw</div>
+   </div>
+  </template>
+
+<script>
+    import HelloWorld from '@/components/HelloWorld'
+
+    export default({
+      name:'Test',
+      data() {
+        return {
+          message: 'Hello Vue! this is my first app',
+          dynamicId:false
+        }
+      },
+      components:{
+       HelloWorld
+      },
+      methods:{
+       resolve:function () {
+          this.message = "magic var"
+       },
+      }
+    })
+</script>
