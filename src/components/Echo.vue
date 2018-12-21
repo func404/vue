f5ffe6f2b607e1e06853761f73f32b0d6a4266e9
Jingxinpo <492971588@qq.com>
2018-12-21 16:09:16 +0800

    test

---
+++ src/components/Echo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     模板输出显示：{{ message }}
+  </div>
+</template>
+
+<script>
+   export default {
+     name:'Echo',
+     props:[
+      'message'
+     ],
+     data() {
+       return {
+          message:message.txt
+       }
+     },
+   }
+</script>
